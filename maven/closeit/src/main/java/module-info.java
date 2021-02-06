0196ccecb6282e37074d527e7ba4f5ddf4ed824c
RichardRoda <RichardRoda@users.noreply.github.com>
2021-02-05 23:42:28 -0500

    Add documentation to the module-info.java, explaining its purpose, stating that the library remains binary compatable with JDK 8, and noting the version number of 1.6.

--- maven/closeit/src/main/java/module-info.java
+++ maven/closeit/src/main/java/module-info.java
@@ -1,3 +1,11 @@
+/**
+ * This defines the module name and exported package when used
+ * as a module in a JDK 9+ environment.  This
+ * library remains binary compatible with JDK 8.
+ *
+ * @author Richard Roda
+ * @since 1.6
+ */
 module com.github.richardroda.util.closeit {
     exports com.github.richardroda.util.closeit;
 }
